1ef7fa329b1bf4682dcd4e532455b3c294f00c3e
unknown <tny23@clc.fitus.edu.vn>
2025-12-09 22:54:41 +0700

    feat: Add .gitignore and initial main.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',           # Tên đăng nhập MySQL (thường là root)
-    'password': 'admin', # <--- NHẬP PASS MYSQL CỦA MÁY BẠN VÀO ĐÂY
-    'database': 'coding_practice_system'      # Phải trùng tên với database vừa tạo ở Bước 1
-}
