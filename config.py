5e5b6599109ad53ecb15ffd7ebf7ccb51ec7dace
jpnosxery <23127435@student.hcmus.edu.vn>
2025-12-09 22:43:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'root',           # Tên đăng nhập MySQL (thường là root)
-    'password': 'YourStrong@Passw0rd', # <--- NHẬP PASS MYSQL CỦA MÁY BẠN VÀO ĐÂY
+    'password': 'admin', # <--- NHẬP PASS MYSQL CỦA MÁY BẠN VÀO ĐÂY
     'database': 'coding_practice_system'      # Phải trùng tên với database vừa tạo ở Bước 1
 }
