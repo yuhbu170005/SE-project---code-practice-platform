3626dc333757a1ba18b570d098fa5c881da341f4
Nguyen Kim Ngan <nguyenkimngan@Nguyens-MacBook-Pro.local>
2026-01-11 23:12:01 +0700

    chore: update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ load_dotenv()
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", ""),
+    "password": os.getenv("DB_PASSWORD", "YourStrong@Passw0rd"),
     "database": os.getenv("DB_NAME", "coding_practice_system"),
 }
